0d49371634a08d12935c30db8c9452584ba2475d
Uchiha0Itatchi <95933910+Uchiha0Itatchi@users.noreply.github.com>
2021-12-10 22:57:52 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,60 @@
+<div class="tags-cloud">
+        <div class="container">
+            <a href="#" class="polylang-badge">2023</a>
+            <a href="#" class="polylang-badge">2022</a>
+            <a href="#" class="polylang-badge">2021</a>
+            <a href="#" class="polylang-badge">2020</a>
+            <a href="#" class="polylang-badge">2019</a>
+            <a href="#" class="polylang-badge">2018</a>
+            <a href="#" class="polylang-badge">2017</a>
+            <a href="#" class="polylang-badge">2016</a>
+            <a href="#" class="polylang-badge">2015</a>
+            <a href="#" class="polylang-badge">2014</a>
+            <a href="#" class="polylang-badge">2013</a>
+            <a href="#" class="polylang-badge">2012</a>
+            <a href="#" class="polylang-badge">2011</a>
+            <a href="#" class="polylang-badge">2010</a>
+            <a href="#" class="polylang-badge">2009</a>
+            <a href="#" class="polylang-badge">2008</a>
+            <a href="#" class="polylang-badge">2007</a>
+            <a href="#" class="polylang-badge">2006</a>
+            <a href="#" class="polylang-badge">2005</a>
+            <a href="#" class="polylang-badge">2004</a>
+            <a href="#" class="polylang-badge">2003</a>
+            <a href="#" class="polylang-badge">2002</a>
+            <a href="#" class="polylang-badge">2001</a>
+            <a href="#" class="polylang-badge">2000</a>
+        </div>
+    </div>
+
+
+    <div class="tornado-header main-header endfooter">
+        <div class="container">
+            <div class="copyrigts">
+                <div class="site">جميع الحقوق محفوظة لـ دراغينيل - تحميل ومشاهدة اون لاين © 2021</div>
+            </div>
+        </div>
+    </div>
+
+
+    
+
+
+
+
+
+
+
+    <script src="<?php echo app1; ?>app1.js"></script>
+    <script src="<?php echo app2; ?>app2.js"></script>
+    <script src="<?php echo app3; ?>app3.js"></script>
+    <script src="<?php echo app4; ?>app4.js"></script>
+    <script src="<?php echo app5; ?>app5.js"></script>
+    <script src="<?php echo app6; ?>app6.js"></script>
+    <script src="<?php echo app7; ?>app7.js"></script>
+    <script src="<?php echo app8; ?>app8.js"></script>
+    <script src="<?php echo app9; ?>app9.js"></script>
+
+
+</body>
+        </html>
