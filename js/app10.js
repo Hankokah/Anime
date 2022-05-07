a2019b51ed278d152f97d86a4d7aea4c3feb8f3d
Uchiha0Itatchi <95933910+Uchiha0Itatchi@users.noreply.github.com>
2022-05-07 05:16:01 +0200

    Delete js directory

--- js/app10.js
+++
@@ -1,9 +0,0 @@
-function change() {
-    document.getElementById("episodes").classList.add('active');
-
-
-    document.getElementById("seasons").classList.remove('active');
-}
-if(change() ) {
-    document.getElementById("holds").style.display = "block";
-}
